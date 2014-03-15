0e02f7bc7572c1300428cd86fb105eed0fd16409
oleg-panichev <oleg.panichev@ukr.net>
2014-03-15 12:25:46 +0200

    Initial commit

--- src/main/java/SpringLesson/App.java
+++ src/main/java/SpringLesson/App.java
@@ -11,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        // context - all beans that are controlled by spring
         ApplicationContext context=new FileSystemXmlApplicationContext("src\\main\\java\\spring-config.xml");
         Application app=context.getBean(Application.class);
         app.start();
