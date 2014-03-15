5bec96fa22ff231273bfa12aa912fad4a81977ed
oleg-panichev <oleg.panichev@ukr.net>
2014-03-15 12:45:38 +0200

    Initial commit

---
+++ src/main/java/SpringLesson/AdvancedChatBot.java
@@ -0,0 +1,19 @@
+package SpringLesson;
+
+import java.util.List;
+
+/**
+ * Created by Oleg on 15.03.14.
+ */
+public class AdvancedChatBot implements GreetingService {
+    private List<String> messages;
+    @Override
+    public void sendGreetings() {
+        if(!messages.isEmpty())
+            System.out.println(messages.remove(0));
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+}
